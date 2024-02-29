0ee17c43bf5c9fa267947e84622b093e37fe9e12
umermuhammad015 <153215851+umermuhammad015@users.noreply.github.com>
2024-02-29 22:40:06 +0500

    v2

--- app/db.ts
+++ app/db.ts
@@ -4,14 +4,31 @@ const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined
+// }
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
